e0fe6bd82ad581fa3f8bae86108ec758235da998
Artemy Bulavin <artemy@live.co.uk>
2020-03-27 15:34:36 +0000

    otp run script and .jar

---
+++ otp/run.sh
@@ -0,0 +1,2 @@
+java -Xmx8G -jar ./otp-1.4.0-shaded.jar --build . --inMemory --port 8080 --securePort 8082 &
+java -Xmx8G -jar ./otp-1.4.0-shaded.jar --build . --inMemory --port 8081 --securePort 8083
