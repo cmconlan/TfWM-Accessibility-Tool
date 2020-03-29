630d2e7022e5135950697f7257f194eca219a5de
Artemy Bulavin <artemy@live.co.uk>
2020-03-29 12:04:46 +0100

    mvoe files

--- www/run.py
+++
@@ -1,6 +0,0 @@
-import os
-from app import app
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7161))
-    app.run(host='0.0.0.0', port=port, debug=True)
--- www/wsgi.py
+++
@@ -1,7 +0,0 @@
-import sys
-import site
-
-site.addsitedir('/dcs/project/transport-access-tool/TfWM-Accessibility-Tool/venv/lib/python3.6/site-packages')
-sys.path.insert(0, '/dcs/project/transport-access-tool/TfWM-Accessibility-Tool/www')
-
-from app import app as application
