175d21b5ffb43774c9e264980a95f2510e044b84
Artemy Bulavin <artemy@live.co.uk>
2020-03-31 11:23:52 +0100

    Integrate SQLite database into Flask App

--- src/www/config.py
+++ src/www/config.py
@@ -6,3 +6,5 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     PORT = os.environ.get('PORT') or 7161
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'tfwm.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
