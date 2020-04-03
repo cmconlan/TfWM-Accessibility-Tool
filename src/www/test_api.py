4e2bd3eb44e0f6f42efcb63c5afa7b9d36d3a48d
Artemy Bulavin <artemy@live.co.uk>
2020-04-03 14:15:51 +0100

    Implement tests for all /meta endpoints

--- src/www/test_api.py
+++ src/www/test_api.py
@@ -1,7 +1,8 @@
+import json
+from functools import reduce
 import pytest
 import mysql.connector as mariadb
-import json
-from app import app
+from app import app, db
 
 
 @pytest.fixture
@@ -12,142 +13,73 @@ def client():
         yield client
 
 
-def execute_query(query: str):
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute(query)
-    data = cursor.fetchall()
-    conn.close()
-    return data
+def test_meta_accessibility_metrics(client):
+    [create_statement] = fetch_expected("""SELECT sql FROM sqlite_master 
+        WHERE tbl_name = 'otp_results_summary' AND type = 'table'""")
 
-
-def humanise(string, prefix):
-    string = string.replace(prefix, '')
-    string_list = string.split('_')
-    return_string = ''
-    for substring in string_list:
-        return_string += substring[0].upper() + substring[1:] + ' '
-    return return_string.strip()
-
-
-def get_key_value_pair(key, prefix):
-    return { 'key': key, 'value': humanise(key, prefix) }
-
-
-def find_prefix(strs):
-    longest_pre = ""
-    if not strs:
-        return longest_pre
-    strs = [string for (string,) in strs]
-    shortest_str = min(strs, key=len)
-    for i in range(len(shortest_str)):
-        if all([x.startswith(shortest_str[:i+1]) for x in strs]):
-            longest_pre = shortest_str[:i+1]
-        else:
-            break
-    return longest_pre
-
-
-def get_key_value_pair_list(query_response):
-    prefix = find_prefix(query_response)
-    expected = []
-    for row in query_response:
-        expected.append(get_key_value_pair(row[0], prefix))
-    return expected
-
-
-def test_humanise():
-    assert humanise('eth_all', 'eth_') == 'All'
-    assert humanise('eth_white', 'eth_') == 'White'
-    assert humanise('eth_gypsy', 'eth_') == 'Gypsy'
-    assert humanise('eth_mixed', 'eth_') == 'Mixed'
-    assert humanise('eth_asian_indian', 'eth_') == 'Asian Indian'
-    assert humanise('PerfectlyNormal', '') == "PerfectlyNormal"
-
-
-def test_doc_root(client):
-    response = client.get('/')
-    assert b'Hello world' in response.data
-
-
-def test_meta_ethnicity(client):
-    ethnicities = execute_query(
-        """SELECT column_name FROM information_schema.columns 
-        WHERE column_name LIKE 'eth%'"""
-    )
-    expected = get_key_value_pair_list(ethnicities)
-    response = client.get("/meta/ethnicity")
-    response_list = json.loads(response.data)
-    assert response_list == expected
-
-
-def test_meta_employment_status(client):
-    statuses = execute_query(
-        """SELECT column_name FROM information_schema.columns 
-        WHERE column_name LIKE 'emp_%' AND column_name<>'EMPTY_QUERIES'"""
-    )
-    expected = get_key_value_pair_list(statuses)
-    response = client.get("/meta/employment-status")
-    response_list = json.loads(response.data)
-    assert response_list == expected
-
-
-def test_meta_disability_status(client):
-    statuses = execute_query(
-        """SELECT column_name FROM information_schema.columns WHERE column_name like 'disability%'"""
-    )
-    expected = get_key_value_pair_list(statuses)
-    response = client.get("/meta/disability-status")
-    response_list = json.loads(response.data)
-    assert response_list == expected
-
-
-def test_accessibility_metric(client):
     response = client.get("/meta/accessibility-metric")
-
-
-def test_geographic_area(client):
-    output_areas = execute_query(
-        """SELECT oa_id from oa"""
-    )
-    expected = []
-    for (output_area,) in output_areas:
-        expected.append({
-            'key': output_area,
-            'value': output_area
-        })
-
-    response = client.get("/meta/geographic-area")
     response_list = json.loads(response.data)
-    assert response_list == expected
+    assert response_has_expected_keys(get_response_keys(response_list), create_statement)
 
 
-def test_time_strata(client):
-    strata = execute_query(
-        """SELECT * FROM stratum_types"""
-    )
-    expected = []
-    for (stratum,) in strata:
-        expected.append({
-            'key': stratum,
-            'value': stratum
-        })
+def test_meta_time_strata(client):
+    query = "SELECT DISTINCT stratum FROM trip_strata"
     response = client.get("/meta/time-strata")
-    response_list = json.loads(response.data)
-    assert response_list == expected
+    verify_response_against_query(response, query)
 
 
-def test_poi_type(client):
-    poi_types = execute_query(
-        """SELECT DISTINCT type FROM poi"""
-    )
-    expected = []
-    for (poi_type,) in poi_types:
-        expected.append({
-            'key': poi_type,
-            'value': poi_type
-        })
+def test_meta_demographic(client):
+    query = "SELECT DISTINCT population FROM populations"
+    response = client.get("/meta/demographic")
+    verify_response_against_query(response, query)
 
+
+def test_meta_poi_type(client):
+    query = "SELECT DISTINCT type FROM poi"
     response = client.get("/meta/point-of-interest-type")
+    verify_response_against_query(response, query)
+
+
+def test_meta_population_metrics(client):
+    response = client.get('/meta/population-metric')
+    metrics = json.loads(app.config['POPULATION_METRICS'])
     response_list = json.loads(response.data)
-    assert response_list == expected
+    response_keys = get_response_keys(response_list)
+
+    assert expected_number_of_results(metrics, response_list)
+    assert no_duplicates(response_keys)
+    assert response_has_expected_keys(response_keys, metrics)
+
+
+def verify_response_against_query(response, sql_query):
+    query_results = fetch_expected(sql_query)
+
+    response_list = json.loads(response.data)
+    response_keys = get_response_keys(json.loads(response.data))
+
+    assert expected_number_of_results(query_results, response_list)
+    assert no_duplicates(response_keys)
+    assert response_has_expected_keys(response_keys, query_results)
+
+
+def fetch_expected(sql_query):
+    results = db.engine.execute(sql_query)
+    return [result for (result,) in results]
+
+
+def get_response_keys(response):
+    return [kv['key'] for kv in response]
+
+
+def expected_number_of_results(expected, results):
+    return len(expected) == len(results)
+
+
+def no_duplicates(results):
+    return len(results) == len(set(results))
+
+
+def response_has_expected_keys(results, expected):
+    keys_in_expected = map(lambda key: key in expected, results)
+    all_keys_in_expected = reduce(lambda x, y: x and y, keys_in_expected)
+    return all_keys_in_expected
