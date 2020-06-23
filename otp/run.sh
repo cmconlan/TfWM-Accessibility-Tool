b418329e972b540d551a8f7f85a454d32c7d8cdf
Artemy Bulavin <artemy@live.co.uk>
2020-06-23 16:49:33 +0100

    Change run.sh to use an additional OTP server

--- otp/run.sh
+++ otp/run.sh
@@ -1,2 +1,3 @@
-java -Xmx8G -jar ./otp-1.4.0-shaded.jar --build . --inMemory --port 8080 --securePort 8082 &
-java -Xmx8G -jar ./otp-1.4.0-shaded.jar --build . --inMemory --port 8081 --securePort 8083
+java -Xmx8G -jar ./otp-1.4.0-shaded.jar --build . --inMemory --port 8080 --securePort 8083 &
+java -Xmx8G -jar ./otp-1.4.0-shaded.jar --build . --inMemory --port 8081 --securePort 8084 &
+java -Xmx8G -jar ./otp-1.4.0-shaded.jar --build . --inMemory --port 8082 --securePort 8085 &
