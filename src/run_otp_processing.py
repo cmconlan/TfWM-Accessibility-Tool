9ac1ea05e806f780f64b388ca56e7afabd9cca1f
Artemy Bulavin <artemy@live.co.uk>
2020-06-26 13:52:40 +0100

    Readbility and file handling changes

--- src/run_otp_processing.py
+++ src/run_otp_processing.py
@@ -63,33 +63,28 @@ def valid_response(response: tuple) -> bool:
         return True
 
 
-def write_complete_row(output_csv: csv.writer, completed_row: tuple) -> None:
-    '''Write a row of OTP results to the output CSV file'''
-    writer = csv.writer(output_csv, delimiter=',')
-    writer.writerow(completed_row)
-
-
 def compute_trips(process_id, host_url, offset, limit, input_file, output_dir):
     output_file = os.path.join(output_dir, f'temp_{process_id}.csv')
     print(f"{process_id} on {host_url} for offset {offset} limit {limit} saving to {output_file}")
 
     row_counter = 0
     with open(input_file, 'r') as csv_file:
+        # Usage of DictReader saves having to unpack rows manually, but
+        # requires that the first row of the file has valid, standard headers
+        reader = csv.DictReader(csv_file)
+        csv_section = get_csv_section(reader, offset, limit)
         with open(output_file, 'a', newline='') as output_csv:
-            # Usage of DictReader saves having to unpack rows manually, but
-            # requires that the first row of the file has valid, standard headers
-            reader = csv.DictReader(csv_file)
-            csv_section = get_csv_section(reader, offset, limit)
+            writer = csv.writer(output_csv, delimiter=',')
             for row in csv_section:
                 response = get_otp_response(host_url, row['oa_lat'], row['oa_lon'], row['poi_lat'], row['poi_lon'], row['date'], row['time'])
                 # Attempting to parse a trip where there was no route returns None for trip values.
                 # If the response from OTP is an invalid trip it is skipped
-                if valid_response(response):
-                    complete_row = (row['trip_id'], *response)
-                    write_complete_row(output_csv, complete_row)
-                    row_counter += 1
-                    if row_counter % 1000 == 0:
-                        print(f'Process {process_id} has completed {row_counter} rows. {(row_counter/limit) * 100}% for this process')
+                # if valid_response(response):
+                complete_row = (row['trip_id'], *response)
+                writer.writerow(complete_row)
+                row_counter += 1
+                if row_counter % 1000 == 0:
+                    print(f'Process {process_id} has completed {row_counter} rows. {((row_counter/limit) * 100):.2f}% for this process')
 
     print(f'{process_id} has completed its chunk.')
     return output_file
@@ -97,18 +92,14 @@ def compute_trips(process_id, host_url, offset, limit, input_file, output_dir):
 
 def split_trips(input_file: str, output_dir: str) -> None:
     host, port, processes, otps = settings.get_otp_settings()
-    processes = int(processes)
-    otps = int(otps)
-    port = int(port)
-
     num_trips = num_rows(input_file)
     step_size = get_step_size(num_trips, processes)
     offsets = get_offsets(num_trips, step_size)
     limits = get_limits(processes, step_size)
 
     # Distribute processes between OTP addresses in a circular manner.
-    # E.g for 4 processes, and 2 OTPs, OTP 1 with port 8080 will have 
-    # processes 0, 2, while OTP 2 with port 8081 will have processes 1, 3
+    # E.g for 4 processes and 2 OTPs, OTP 1 with port 8080 will have 
+    # processes 0, 2 while OTP 2 with port 8081 will have processes 1, 3.
     hosts = []
     for i in range(processes):
         hosts.append(f"http://{host}:{str(port + (i % otps))}")
@@ -124,27 +115,33 @@ def split_trips(input_file: str, output_dir: str) -> None:
     data = data.tolist()
 
     start = time.time()
+    # Distribute the OTP processing between processes. Each process returns
+    # the path to the file it wrote its results to.
     with multiprocessing.Pool(int(processes)) as pool:
         results = pool.starmap(compute_trips, [tuple(row) for row in data])
     end = time.time()
     elapsed = end - start
-    print("{}min elapsed.".format(elapsed / 60.0))
+    print(f"{(elapsed / 60.0):.2f}min elapsed.")
     return results
 
 
 def combine_complete_files(output_dir, files):
     '''Combine the individual files produced by each process into a single main file'''
-    output_file = open(os.path.join(output_dir, 'results_full_test.csv'), 'w')
+    output_file_name = os.path.join(output_dir, 'results_full.csv')
+    output_file = open(output_file_name, 'w')
+    print("Combining results to: " + output_file_name)
     output_csv = csv.writer(output_file)
     for csv_file in files:
         with open(csv_file, newline='') as f:
             reader = csv.reader(f)
             for index, line in enumerate(reader):
                 output_csv.writerow(line)
+    output_file.close()
 
 
 def cleanup(complete_files):
     '''Delete the temp files produced by each process'''
+    print("Cleaning up...")
     for f in complete_files:
         os.remove(f)
 
