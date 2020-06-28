11accb4047d85e0b3a0893110dcc630b2d95a343
Artemy Bulavin <artemy@live.co.uk>
2020-06-28 13:44:57 +0100

    Change run.sh to disbale logs of last OTP in script

--- otp/run.sh
+++ otp/run.sh
@@ -1,3 +1,3 @@
 java -Xmx8G -jar ./otp-1.4.0-shaded.jar --build . --inMemory --port 8080 --securePort 8083 &
 java -Xmx8G -jar ./otp-1.4.0-shaded.jar --build . --inMemory --port 8081 --securePort 8084 &
-java -Xmx8G -jar ./otp-1.4.0-shaded.jar --build . --inMemory --port 8082 --securePort 8085 &
+java -Xmx8G -jar ./otp-1.4.0-shaded.jar --build . --inMemory --port 8082 --securePort 8085
