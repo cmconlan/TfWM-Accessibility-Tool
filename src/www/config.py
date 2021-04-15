297abc8e1b2a312db0c2327d6805fad2ad1a16ed
Louis Irwin <coding@louisirwin.co.uk>
2021-04-15 17:25:45 +0100

    Remove print statement

--- src/www/config.py
+++ src/www/config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
-print(os.path.join(basedir, '.env'))
 
 class Config(object):
     PORT = os.environ.get('PORT') or 7161
